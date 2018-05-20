3e5e51ec5f9dece1e2203cd74dd05c2b08a4d610
DavidPhantom <39460587+DavidPhantom@users.noreply.github.com>
2018-05-20 17:21:25 +0300

    Add files via upload

--- Project2/main.cs
+++ Project2/main.cs
@@ -1,27 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Linq;
 using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Forms;
-using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Drawing.Printing;
-using Emgu.CV;
-using Emgu.CV.UI;
-using Emgu.CV.CvEnum;
-using Emgu.CV.OCR;
-using Emgu.CV.Structure;
-using Emgu.CV.Util;
-using Emgu.Util;
-using Emgu.CV.Cvb;
+using System.Threading.Tasks;
 
 namespace Project2
 {
@@ -29,8 +10,8 @@ namespace Project2
     {
         static void Main(string[] args)
         {
-            var parser = new Parser();
-            var output = parser.Parse("001.gif");
+            var shapeD = new shape();
+            var output = shapeD.Shape("001.gif");
             Console.WriteLine(output);
             Console.ReadKey();
         }
---
+++ Project2/shape.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+using Emgu.CV.OCR;
+
+namespace Project2
+{
+    public class shape
+    {
+        public string Shape(string filename)
+        {
+            StringBuilder msgBuilder = new StringBuilder("Performance: ");
+
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(filename);
+
+            Stopwatch watch = Stopwatch.StartNew();
+            double cannyThreshold = 180.0;
+            
+            #region Canny and edge detection
+            watch.Reset(); watch.Start();
+            double cannyThresholdLinking = 120.0;
+            UMat cannyEdges = new UMat();
+            CvInvoke.Canny(img, cannyEdges, cannyThreshold, cannyThresholdLinking);
+            cannyEdges.Save("cannyEdges.png");
+
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(cannyEdges, 1, Math.PI/45.0, 20, 0, 0);
+
+            watch.Stop();
+            msgBuilder.Append(String.Format("Canny & Hough lines - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            #region Find triangles and rectangles
+            watch.Reset(); watch.Start();
+            List<RotatedRect> boxList = new List<RotatedRect>(); 
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > 250) 
+                        {
+                            if (approxContour.Size == 4) 
+                            {
+                                #region determine if all the angles in the contour are within [80, 100] degree
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+                                #endregion
+
+                                if (isRectangle) boxList.Add(CvInvoke.MinAreaRect(approxContour));
+                            }
+                        }
+                    }
+                }
+            }
+
+            watch.Stop();
+            msgBuilder.Append(String.Format("Rectangles - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            var originalImageBox = new ImageBox { Image = img };
+            var result = msgBuilder.ToString();
+            List<IInputOutputArray> licensePlateImagesList = new List<IInputOutputArray>();
+
+            #region draw rectangles
+            Image<Bgr, Byte> triangleRectangleImage = img.CopyBlank();
+            foreach (RotatedRect box in boxList)
+            {
+                licensePlateImagesList.Add(triangleRectangleImage);
+                triangleRectangleImage.Draw(box, new Bgr(Color.DarkOrange), 2);
+            }
+
+            var lic = new licPlateRecog.LicensePlateDetector();
+            var licoutput = lic.DetectLicensePlate(img, licensePlateImagesList, licensePlateImagesList, boxList);
+
+            triangleRectangleImage.Save("rectangles.png");
+            #endregion
+
+            #region draw lines
+            Image<Bgr, Byte> lineImage = img.CopyBlank();
+            foreach (LineSegment2D line in lines)
+                lineImage.Draw(line, new Bgr(Color.Green), 2);
+            lineImage.Save("lines.png");
+            #endregion
+
+            return result;
+        }
+
+    }
+}
